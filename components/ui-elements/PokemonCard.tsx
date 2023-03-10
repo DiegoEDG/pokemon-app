9a9e56bd5106258d4097ff50ab0deb2744413520
Digudev <1.5.dg.diego@gmail.com>
2023-03-10 10:41:46 -0600

    Fix: Grid size modified

--- components/ui-elements/PokemonCard.tsx
+++ components/ui-elements/PokemonCard.tsx
@@ -16,7 +16,7 @@ const PokemonCard: FC<Props> = ({ pokemon }) => {
 	};
 
 	return (
-		<Grid xs={6} sm={3} md={2} xl={1} key={pokemon.id}>
+		<Grid xs={6} sm={3} md={2} xl={2} key={pokemon.id}>
 			<Card isHoverable isPressable variant="bordered" onClick={onClick}>
 				<Card.Body css={{ p: 1 }}>
 					<Card.Image src={pokemon.img} width="100%" height={140} css={{ paddingTop: 20 }} />
