0571f6b38427fc0b278a43ffe3a4ca01d5c1c7fa
Digudev <1.5.dg.diego@gmail.com>
2023-03-10 10:41:08 -0600

    Router: Links added to navbar

--- components/ui-elements/Navbar.tsx
+++ components/ui-elements/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Spacer, Text, useTheme } from '@nextui-org/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 export const Navbar = () => {
@@ -7,21 +8,25 @@ export const Navbar = () => {
 
 	return (
 		<div className={styles.navbar} style={{ backgroundColor: theme?.colors.gray50.value }}>
-			<Image
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/93.png"
-				alt="Logo"
-				width={96}
-				height={96}
-			/>
-			<Text h2>P</Text>
-			<Text h3 className={styles.title}>
-				okemón
-			</Text>
+			<Link href="/" className={styles.link}>
+				<Image
+					src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/93.png"
+					alt="Logo"
+					width={96}
+					height={96}
+				/>
+				<Text h2>P</Text>
+				<Text h3 className={styles.title}>
+					okemón
+				</Text>
+			</Link>
 			<Spacer css={{ flex: '1' }} />
 
-			<Text className={styles.favoritos} h4>
-				Favoritos
-			</Text>
+			<Link href="/favorites" className={styles.link}>
+				<Text className={styles.favoritos} h4>
+					Favorites
+				</Text>
+			</Link>
 		</div>
 	);
 };
