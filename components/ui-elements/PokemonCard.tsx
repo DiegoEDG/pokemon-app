948b1a6f514f02310604d01dab9397a20f01b4df
Digudev <1.5.dg.diego@gmail.com>
2023-03-10 08:53:08 -0600

    Routing: onClick function added to card component

--- components/ui-elements/PokemonCard.tsx
+++ components/ui-elements/PokemonCard.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { FC } from 'react';
 import { Card, Grid, Row, Text } from '@nextui-org/react';
-import { PokemonData } from '../../interfaces';
+import { PokemonMinimunData } from '../../interfaces';
+import { useRouter } from 'next/router';
 
 interface Props {
-	pokemon: PokemonData;
+	pokemon: PokemonMinimunData;
 }
 
 const PokemonCard: FC<Props> = ({ pokemon }) => {
+	const router = useRouter();
+
+	const onClick = () => {
+		router.push(`/pokemon/${pokemon.id}`);
+	};
+
 	return (
 		<Grid xs={6} sm={3} md={2} xl={1} key={pokemon.id}>
-			<Card isHoverable>
+			<Card isHoverable isPressable variant="bordered" onClick={onClick}>
 				<Card.Body css={{ p: 1 }}>
-					<Card.Image src={pokemon.img} width="100%" height={140} />
+					<Card.Image src={pokemon.img} width="100%" height={140} css={{ paddingTop: 20 }} />
 					<Card.Footer>
 						<Row justify="space-between">
 							<Text transform="capitalize">{pokemon.name}</Text>
-							<Text>{pokemon.id}</Text>
+							<Text>{`# ${pokemon.id}`}</Text>
 						</Row>
 					</Card.Footer>
 				</Card.Body>
