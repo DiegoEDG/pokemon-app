d113a7f3653804db1064af46d920f85696ef90af
Digudev <1.5.dg.diego@gmail.com>
2023-03-11 10:04:09 -0600

    Refactoring: Data fetching

--- api/pokeApi.ts
+++ api/pokeApi.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
+import { PokemonResponse } from '../interfaces';
 
 export const pokeApi = axios.create({
 	baseURL: 'https://pokeapi.co/api/v2'
 });
+
+export const getFullPokemonData = async () => {
+	const { data } = await pokeApi.get<PokemonResponse>('/pokemon?limit=151');
+	return data;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
 import { GetStaticProps } from 'next';
 import { Grid } from '@nextui-org/react';
-import { pokeApi } from '../../api';
+import { getFullPokemonData } from '../../api';
 import { MainLayout } from '../../components/layouts';
 import { PokemonCard } from '../../components/ui-elements';
-import { PokemonMinimunData, PokemonResponse } from '../../interfaces';
+import { PokemonMinimunData } from '../../interfaces';
 
 interface Props {
 	pokemons: PokemonMinimunData[];
@@ -23,7 +23,7 @@ const Home: FC<Props> = ({ pokemons }) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-	const { data } = await pokeApi.get<PokemonResponse>('/pokemon?limit=151');
+	const data = await getFullPokemonData();
 
 	const pokemons: PokemonMinimunData[] = data.results.map((pokemon, i) => ({
 		...pokemon,
